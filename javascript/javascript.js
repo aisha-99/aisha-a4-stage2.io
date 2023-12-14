648e0c6b2bb6edbc15fabdf43fe5b48705bf80a6
aisha-99 <123507252+aisha-99@users.noreply.github.com>
2023-12-13 23:44:05 -0500

    Add files via upload

---
+++ javascript/javascript.js
@@ -0,0 +1,11 @@
+
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.classList.contains("responsive")) {
+      x.classList.remove("responsive");
+    } else {
+      x.classList.add("responsive");
+    }
+  }
+  
+
